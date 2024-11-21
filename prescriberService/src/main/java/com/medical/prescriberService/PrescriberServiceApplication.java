1c8f0e1ff73b2bb1cba8b93dffa33fa8c8d1b81d
Rakshit <55257622+rakshit-goel-cse@users.noreply.github.com>
2024-11-21 18:20:45 +0530

    basic setup of 4 services

---
+++ prescriberService/src/main/java/com/medical/prescriberService/PrescriberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medical.prescriberService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriberServiceApplication.class, args);
+	}
+
+}
