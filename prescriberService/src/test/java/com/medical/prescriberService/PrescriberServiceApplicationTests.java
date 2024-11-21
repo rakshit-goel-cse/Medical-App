c16da9b4506cb0939a5bd0aebb25635cd8a025ea
Rakshit <55257622+rakshit-goel-cse@users.noreply.github.com>
2024-11-21 18:31:02 +0530

    base code for patient,prescriber, prescription and drug service

---
+++ prescriberService/src/test/java/com/medical/prescriberService/PrescriberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.prescriberService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ priscriptionService/src/test/java/com/medical/priscriptionService/PriscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.priscriptionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
